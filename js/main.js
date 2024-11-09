7f5e4d1f70c720fa0260a3af302cb23433005fd7
FerbaDev <elferbarron1@gmail.com>
2024-11-09 10:06:30 -0300

    se agrega boton de whatsapp

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const cerrar = document.querySelector("#cerrar");
 const blob1 = document.querySelector("#blob1");
 const frase = document.querySelector(".scale-in-hor-left");
 const logo = document.querySelector("#logo");
+const whatsappButton = document.querySelector(".whatsapp-float");
 
 abrir.addEventListener("click", () => {
   nav.classList.add("visible");
@@ -17,4 +18,16 @@ cerrar.addEventListener("click", () => {
 logo.addEventListener("click", () => {
   //enlace de la pagina que quieras
   window.location.href = "./index.html";
+});
+
+//boton de whatsapp
+window.addEventListener("scroll", function() {
+  console.log("Scroll position:", window.scrollY);
+  if (window.scrollY > window.innerHeight * 0.8) {
+      console.log("Mostrando botón de WhatsApp");
+      whatsappButton.style.display = "flex";
+  } else {
+      console.log("Ocultando botón de WhatsApp");
+      whatsappButton.style.display = "none";
+  }
 });
